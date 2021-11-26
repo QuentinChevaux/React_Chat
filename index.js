12a895518b01c621031eaff695c0bf9c7ec3001f
QuentinChevaux <quentin.chevaux@gmail.com>
2021-11-26 15:39:19 +0100

    Chat

--- index.js
+++ index.js
@@ -3,15 +3,41 @@ class Chat extends React.Component {
     constructor(props) {
         super(props);
         
-        this.state = {login_text: '', is_connected: false}
+        this.state = {login_text: '', is_connected: false, user_input_text: '', tabmess: ["coucou","test"]}
 
         this.enter_login = this.enter_login.bind(this)
+
+        this.check_connexion = this.check_connexion.bind(this)
+
+        this.user_text = this.user_text.bind(this)
+
+        this.message_sent = this.message_sent.bind(this)
 
     }
 
     enter_login(event) {
 
-        this.setState({login_text: event.target.login_text})
+        this.setState({login_text: event.target.value})
+
+    }
+
+    check_connexion(event) {
+
+        this.setState({ is_connected: !this.state.is_connected })
+
+    }
+
+    user_text(event) {
+
+        this.setState( {user_input_text: event.target.value})
+
+    }
+
+    message_sent(event) {
+
+        this.setState({  })
+
+        this.setState({ user_input_text: '' })
 
     }
 
@@ -19,21 +45,46 @@ class Chat extends React.Component {
 
         return ( <div>
 
-            <Header login_value = {this.state.login_text} change_login_text = {this.enter_login} />
-            <Middle_page />
+            <Header login_value = {this.state.login_text} change_login_text = {this.enter_login} if_connected = {this.state.is_connected} change_connexion = {this.check_connexion} />
+            <Middle_page if_connected = {this.state.is_connected} typing = {this.state.user_input_text} user_typed_text = {this.user_text} message_sent = {this.message_sent}
+                         tabmess = {this.state.tabmess}/>
             <Footer />
 
         </div>
-
 
         )
 
     }
     
-
 }
 
 function Header(props) {
+
+    let checking_logged
+
+    if ( props.if_connected == false ) {
+
+        checking_logged = <React.Fragment>
+
+            <input type='text' placeholder='Entrez Votre Pseudo : ...' id='login_input' value = {props.login_value} onChange = {props.change_login_text} />
+
+            <a href='#' id='login_button' onClick = {props.change_connexion} >Login</a> 
+
+        </React.Fragment>
+
+    }
+
+    else {
+
+        checking_logged = <React.Fragment>
+
+            <p>{props.login_value}</p>
+
+            <a href='#' id='logout_button' onClick = {props.change_connexion} >Logout</a> 
+
+        </React.Fragment>
+
+    }
 
     return <div className='flex_between header' >
 
@@ -43,16 +94,11 @@ function Header(props) {
 
         </div>
 
-        <div>
-
-            <input type='text' placeholder='Entrez Votre Pseudo : ...' id='login_input' value = {props.login_value} onChange = {props.change_login_text} />
-
-            <a href='#' id='login_button'>Login</a> 
-
-            <p>{props.login_text}</p>
+        <div className='flex'>
+        
+            {checking_logged}
 
         </div>
-
 
     </div>
 
@@ -64,7 +110,9 @@ function Middle_page(props) {
 
         <Left_online_status />
 
-        <Chat_display_and_type />
+        <Chat_display_and_type if_connected = {props.if_connected} typing = {props.typing}  user_typed_text = {props.user_typed_text} message_sent = {props.message_sent}
+            tabmess = {props.tabmess}
+        />
 
     </div>
 
@@ -83,13 +131,12 @@ function Left_online_status(props) {
 
 
 function Chat_display_and_type(props) {
-
-    
+   
 
     return <div className='w-75'>
 
-        <Chat_display />
-        <Chat_box />
+        <Chat_display typing = {props.typing}  user_typed_text = {props.user_typed_text} tabmess = {props.tabmess} />
+        <Chat_box if_connected = {props.if_connected} typing = {props.typing}  user_typed_text = {props.user_typed_text} message_sent = {props.message_sent}/>
 
     </div>
 
@@ -97,13 +144,13 @@ function Chat_display_and_type(props) {
 
 function Chat_display(props) {
 
+    console.log(props.typing)
+
     return <div className='bg_blue chat_display'>
 
         <div>
 
-            <h2 className='chat_display_text'>Chat Display box</h2>
-
-            <div className='text'></div>
+            <div className='chat_display_text'>{props.tabmess.map((elem, key) => elem + ' ')} </div>
 
         </div>
 
@@ -113,18 +160,27 @@ function Chat_display(props) {
 
 function Chat_box(props) {
 
+    // console.log(props.typing);
+
+    let textareaclass = ''
+
+    if ( props.if_connected == false) {
+
+        textareaclass = 'display_none'
+
+    }
 
     return <div className='flex bg_red chat_box'>
 
         <div>
 
-            <input type='text' id='input_chat_box' placeholder='Tapez pour Discuter ...' />
+            <input type='text' id='input_chat_box' placeholder='Tapez pour Discuter ...' className={textareaclass} value = {props.typing} onChange = {props.user_typed_text}/>
 
         </div>
 
         <div>
 
-            <a href='#' id='send_message_button'>Envoyer</a>
+            <a href='#' id='send_message_button' className={textareaclass} onClick = {props.message_sent} >Envoyer</a>
 
         </div>
 
